b4f9818513b7f0f46ffebb8c7ef373536c117a18
KDaria <dkdva@mail.ru>
2022-11-24 20:43:19 +0100

    add index.js

--- src/index.js
+++ src/index.js
@@ -23,39 +23,3 @@ const calc = () => {
 
 //export default ;
 
-
-
-
-
-
-# Brain Games
-[![Maintainability](https://api.codeclimate.com/v1/badges/d2b0b21700faea5f3d67/maintainability)](https://codeclimate.com/github/victorlitvinenko/frontend-project-lvl1/maintainability)
-[![Build Status](https://travis-ci.org/victorlitvinenko/frontend-project-lvl1.svg?branch=master)](https://travis-ci.org/victorlitvinenko/frontend-project-lvl1)
-
-## Getting started
-```sudo npm install -g frontend-victorlitvinenko-lvl1```
-
-### Even numbers
-```brain-even```
-
-[![asciicast](https://asciinema.org/a/NEz7RFNIz4nT2tXqiNrH7JOo8.svg)](https://asciinema.org/a/NEz7RFNIz4nT2tXqiNrH7JOo8)
-
-### Result of the expression
-```brain-calc```
-
-[![asciicast](https://asciinema.org/a/MWaI8YXEyIqoizSDcfmTqOohZ.svg)](https://asciinema.org/a/MWaI8YXEyIqoizSDcfmTqOohZ)
-
-### Result of the expression
-```brain-gcd```
-
-[![asciicast](https://asciinema.org/a/OgX2PWRHjG68a1tBTxk90pNjm.svg)](https://asciinema.org/a/OgX2PWRHjG68a1tBTxk90pNjm)
-
-### Missing numbers in the progression
-```brain-progression```
-
-[![asciicast](https://asciinema.org/a/3wDqeVywWNn8VhUB2jbcjUbVo.svg)](https://asciinema.org/a/3wDqeVywWNn8VhUB2jbcjUbVo)
-
-### Prime numbers
-```brain-prime```
-
-[![asciicast](https://asciinema.org/a/jhTlOQ962YHLoiFjESH9IxJN4.svg)](https://asciinema.org/a/jhTlOQ962YHLoiFjESH9IxJN4)
