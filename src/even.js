43d5976d78f36fdde131bc8e09fe939e9c6af445
KDaria <dkdva@mail.ru>
2022-11-24 16:51:56 +0100

    Add a new game (eventGame)

---
+++ src/even.js
@@ -0,0 +1,27 @@
+import readlineSync from "readline-sync";
+
+const evenGame = () => {
+  const userName = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${userName}!`);
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    let resultGame;
+    const randomNumber = Math.round(Math.random() * 10);
+    console.log(`${"Question:"} ${randomNumber}`);
+    const answer = readlineSync.question("Your answer: ");
+
+    const correctAnswer = randomNumber % 2 === 0 ? "yes" : "no";
+    if (answer === correctAnswer) {
+      console.log((resultGame = "Correct!"));
+    } else {
+      resultGame = `"${answer}" is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`;
+      console.log(resultGame);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default evenGame;
